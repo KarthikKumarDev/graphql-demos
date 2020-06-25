83a03325a38ec506a38685911c5b01e8ef1f7c3c
Karthik Kumar <jkkr.1996@gmail.com>
2020-06-25 21:25:55 +0530

    Add concurrently json-server and remove lodash

--- mock-linked-in/schema/schema.js
+++ mock-linked-in/schema/schema.js
@@ -1,14 +1,11 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-const _ = require("lodash");
-
-const users = [
-  { id: "u001", firstName: "Raj", age: 55 },
-  { id: "u002", firstName: "Hari", age: 32 },
-  { id: "u003", firstName: "Ameer", age: 88 },
-];
+const axios = require("axios");
+const { response } = require("express");
 
 // The GraphQLObjectType is used to define the data and their properties
+
+const dataUrl = "http:\\\\localhost:3000";
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -26,7 +23,9 @@ const RootQueryType = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`${dataUrl}\\users\\${args.id}`)
+          .then((response) => response.data);
       },
     },
   },
